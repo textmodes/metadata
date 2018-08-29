83b4964279e563866a5e1ccc7bcf330a04df993f
maze <maze@booking.com>
2018-08-29 19:04:54 +0200

    More strict social checks

--- test/artist/main.go
+++ test/artist/main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"net/url"
 	"os"
+	"regexp"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -32,10 +34,68 @@ func test(name string) bool {
 		return false
 	}
 
+	if artist.Aliases != nil {
+		for _, slug := range artist.Aliases {
+			if !testSlug(slug) {
+				fmt.Fprintf(os.Stderr, "FAIL %s: invalid slug %q\n", name, slug)
+				return false
+			}
+		}
+	}
+
+	if artist.Social != nil {
+		for site, value := range artist.Social {
+			if err = testSocial(site, value); err != nil {
+				fmt.Fprintf(os.Stderr, "FAIL %s: %v\n", name, err)
+				return false
+			}
+		}
+	}
+
 	if verbose {
 		fmt.Printf("GOOD %s\n", name)
 	}
 	return true
+}
+
+func testSlug(slug string) bool {
+	// TODO(maze): more elaborate testing
+	return url.PathEscape(slug) == slug
+}
+
+var (
+	nonEmpty = regexp.MustCompile(`.+`)
+	numeric  = regexp.MustCompile(`^\d+$`)
+	social   = map[string]*regexp.Regexp{
+		"artcity":         numeric,
+		"behance":         nonEmpty,
+		"csdb":            numeric,
+		"demozoo":         numeric,
+		"deviantart":      nonEmpty,
+		"facebook":        nonEmpty,
+		"flickr":          regexp.MustCompile(`^\d+@N\d+$`),
+		"github":          regexp.MustCompile(`^\w[-\w]*$`),
+		"google+":         regexp.MustCompile(`^(?:\+\w*|\d+)$`),
+		"instagram":       nonEmpty,
+		"linkedin":        regexp.MustCompile(`^\w[-\w]*$`),
+		"pinterest":       nonEmpty,
+		"pouet":           numeric,
+		"twitter":         nonEmpty,
+		"vimeo":           regexp.MustCompile(`^[a-zA-Z]\w+$`),
+		"youtube":         regexp.MustCompile(`^[a-zA-Z]\w+$`),
+		"youtube-channel": regexp.MustCompile(`^[-A-Za-z0-9+/]+$`),
+	}
+)
+
+func testSocial(site, value string) error {
+	re, found := social[site]
+	if !found {
+		return fmt.Errorf("social site %q invalid", site)
+	}
+	if !re.MatchString(value) {
+		return fmt.Errorf("social site %s value %q invalid", site, value)
+	}
+	return nil
 }
 
 var verbose bool
